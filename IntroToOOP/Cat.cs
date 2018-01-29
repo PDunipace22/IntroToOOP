d2923988f330a844ba028238991bb2efb43ee577
PDunipace22 <peterdunipace@gmail.com>
2018-01-29 11:40:47 -0500

    added Cat classes and attributes

--- IntroToOOP/Cat.cs
+++ IntroToOOP/Cat.cs
@@ -8,5 +8,21 @@ namespace IntroToOOP
 {
     class Cat
     {
+        //I need STATES
+
+        //fields
+        private string name;
+
+
+
+
+        //I need BEHAVIORS
+
+        //Constructors
+        public Cat()
+        {
+            //default constructor
+            //takes no parameters(nothing in the parentheses)
+        }
     }
 }
