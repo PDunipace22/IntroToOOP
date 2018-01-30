5b1156a78d6870b93dc12c1b18c77fc0b2c50d52
PDunipace22 <peterdunipace@gmail.com>
2018-01-30 09:11:53 -0500

    added SuperHero class

---
+++ IntroToOOP/SuperHero.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroToOOP
+{
+    class SuperHero
+    {
+        private string name;
+        private string powerBoost;
+        private string health;
+        private string strengthLevel;
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+        public string PowerBoost
+        {
+            get { return this.powerBoost; }
+            set { this.powerBoost = value; }
+        }
+        public string Health
+        {
+            get { return this.health; }
+            set { this.health = value; }
+        }
+        public string StrengthLevel
+        {
+            get { return this.strengthLevel; }
+            set { this.strengthLevel = value; }
+        }
+
+        public SuperHero()
+        {
+
+        }
+        public SuperHero()
+        {
+
+        }
+        public SuperHero()
+        {
+
+        }
+    }
+}
